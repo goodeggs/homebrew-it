28ab434169ae33d891ffe3384964f9e4752c2d76
Bob Zoller <bob@zoller.us>
2015-07-27 13:35:47 -0700

    Assume GAM secrets live in the Good Eggs Dropbox
    this is a little brittle, but will work in our current
    environment just fine.

--- Formula/gam.rb
+++ Formula/gam.rb
@@ -2,12 +2,14 @@
 class Gam < Formula
   homepage "https://github.com/jay0lee/GAM"
   url "https://github.com/jay0lee/GAM/archive/v3.45.tar.gz"
-  version "3.45-1"
+  version "3.45-2"
   sha256 "986d10ff134996de071633f34dc4d0d929844ed6bb896e168f818b43b107ce73"
   head "https://github.com/jay0lee/GAM.git"
 
   def install
     prefix.install Dir["*"]
+    prefix.install_symlink "/Users/#{ENV["USER"]}/Dropbox (Good Eggs)/GAM/client_secrets.json" => "client_secrets.json"
+    prefix.install_symlink "/Users/#{ENV["USER"]}/Dropbox (Good Eggs)/GAM/oauth2service.json" => "oauth2service.json"
     bin.install_symlink prefix/"gam.py" => "gam"
   end
 
